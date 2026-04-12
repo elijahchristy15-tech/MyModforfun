175ba2075ee568029e1ef19667cddaa1a637c4e8
elijahchristy15-tech <elijahchristy15@gmail.com>
2026-04-12 15:27:24 -0700

    adding items

--- src/main/java/net/eli/tutorialmod/TutorialMod.java
+++ src/main/java/net/eli/tutorialmod/TutorialMod.java
@@ -1,6 +1,8 @@
 package net.eli.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.eli.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -33,6 +35,8 @@ public class TutorialMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -44,8 +48,12 @@ public class TutorialMod {
     { }
 
     // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event)
-    {}
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.DIERALD);
+            event.accept(ModItems.RAW_DIERALD);
+        }
+    }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
---
+++ src/main/java/net/eli/tutorialmod/item/ModItems.java
@@ -0,0 +1,25 @@
+package net.eli.tutorialmod.item;
+
+import net.eli.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    //registering items
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> DIERALD = ITEMS.register("dierald",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_DIERALD = ITEMS.register("raw_dierald",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
